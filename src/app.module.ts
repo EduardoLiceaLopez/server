a4889ea60b9655aecf06206f0990c40801d92688
EduardoLiceaLopez <eduardo.lc.lz12@gmail.com>
2023-03-07 18:53:47 -0600

    q?

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path'; //Puede unir dos directorios
+
+@Module({
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+                          //ruta actual
+  }) 
+  ,UsersModule],
+
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
