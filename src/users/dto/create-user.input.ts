ca0f99a6926b1ef8b44077fc22a27c83cb85ff1a
EduardoLiceaLopez <eduardo.lc.lz12@gmail.com>
2023-03-08 21:15:35 -0600

    Ultimo avance

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,5 +1,5 @@
 import { Field, InputType, Int } from "@nestjs/graphql";
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, MaxLength, MinLength } from "class-validator";
 
     @InputType()
     export class CreateUserInput{
@@ -8,29 +8,26 @@ import { IsNotEmpty } from "class-validator";
     @Field()
     name: string;
 
-
     @Field()
     middle_name: string;
     
-
     @Field({nullable: true})
     last_name?: string;
-    
 
+    @MaxLength(18, {message: 'El CURP se conforma máximo 18 carcateres'})
+    @MinLength(18, {message: 'EL CURP se conforma por mámixo 18 caracteres'})
     @Field()
     curp: string;
-    
 
+    @MinLength(13, {message: 'El RFC se conforma máximo de 13 carcateres'})
+    @MaxLength(13, {message: 'El RFC se conforma máximo 13 carcateres'})
     @Field()
     rfc: string;
     
-
     @Field((type) => Int)
     phone_number: number;
     
-
     @Field()
     email: string;
-
 
 }
